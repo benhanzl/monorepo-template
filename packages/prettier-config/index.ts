55dbdcc9aaf8a7d3a81d4c8efca23d1ce9e64b59
Ben Hanzl <ben.hanzl@gmail.com>
2026-01-08 22:13:35 -0500

    chore: add shared prettier config

---
+++ packages/prettier-config/index.ts
@@ -0,0 +1,13 @@
+import type { Config } from "prettier";
+
+const config: Config = {
+  arrowParens: "always",
+  endOfLine: "lf",
+  printWidth: 100,
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "all",
+};
+
+export default config;
